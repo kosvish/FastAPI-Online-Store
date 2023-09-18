17270b22714a79605025d064d83bcb708800685e
mrkoc <mr.kocta@bk.ru>
2023-09-18 12:56:15 +0300

    add var for checkout static dir.Include router

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from src.auth.base_config import auth_backend, fastapi_users
 from src.schemas.user import UserRead, UserCreate
+from src.pages.router import router as pages_router
+from fastapi.staticfiles import StaticFiles
+
+
 
 app = FastAPI(
     title="Online Store"
 )
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 app.include_router(fastapi_users.get_auth_router(auth_backend),
                    prefix="/auth",
@@ -16,3 +22,5 @@ app.include_router(
     prefix="/auth",
     tags=["Auth"]
 )
+
+app.include_router(pages_router)
