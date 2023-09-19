0acb58eb97b20a7156ec73954c8ab8e66ebc7d78
mrkoc <mr.kocta@bk.ru>
2023-09-19 12:18:27 +0300

    add check auth router

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI
 from src.auth.base_config import auth_backend, fastapi_users
 from src.schemas.user import UserRead, UserCreate
 from src.pages.router import router as pages_router
+from src.endpoints.auth_status import router as auth_router
 from fastapi.staticfiles import StaticFiles
-
-
 
 app = FastAPI(
     title="Online Store"
@@ -19,6 +18,12 @@ app.include_router(fastapi_users.get_auth_router(auth_backend),
 
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"]
+)
+
+app.include_router(
+    auth_router,
     prefix="/auth",
     tags=["Auth"]
 )
