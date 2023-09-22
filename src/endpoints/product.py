fb95a351060c43fb0fb64a5d3d6d99be4e3d924d
mrkoc <mr.kocta@bk.ru>
2023-09-22 12:25:36 +0300

    (create): endpoint to add user_product to db

---
+++ src/endpoints/product.py
@@ -0,0 +1,28 @@
+from src.models.product import Product
+from fastapi import APIRouter, Depends
+from src.schemas.product import ProductCreate
+from src.database import AsyncSession, get_async_session
+from src.models.user import User
+from src.auth.base_config import current_user
+
+
+router = APIRouter(
+    prefix="/product",
+    tags=["Product"]
+)
+
+
+@router.post("/add_product")
+async def add_product(product: ProductCreate, db: AsyncSession = Depends(get_async_session),
+                      user: User = Depends(current_user)):
+
+    async with db as session:
+        db_product = Product(**product.dict(), username=user.username)
+        session.add(db_product)
+        await db.commit()
+        await db.refresh(db_product)
+        return db_product
+
+
+
+
