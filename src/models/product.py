1b09d4ef477eeb290822821403742e4c5c942978
mrkoc <mr.kocta@bk.ru>
2023-09-22 12:23:46 +0300

    (create): declarative product table

--- src/models/product.py
+++ src/models/product.py
@@ -1,4 +1,6 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, Float
+from sqlalchemy import MetaData, Table, Column, Integer, String, Float, ForeignKey
+from src.database import Base
+from src.models.user import user
 
 metadata = MetaData()
 
@@ -10,5 +12,17 @@ product = Table(
     Column("description", String, nullable=False),
     Column("category", String, nullable=False),
     Column("price", Float, nullable=False),
-    Column("brand", String, nullable=False)
+    Column("brand", String, nullable=False),
+    Column("username", ForeignKey(user.c.username), nullable=False)
 )
+
+
+class Product(Base):
+    __tablename__ = "product"
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String, nullable=True)
+    description = Column("description", String, nullable=True)
+    category = Column("category", String, nullable=True)
+    price = Column("price", Float, nullable=True)
+    brand = Column("brand", String, nullable=True)
+    username = Column("username", ForeignKey(user.c.username), nullable=False)
