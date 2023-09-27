d50325c6532afcc4389abc62cf8fdee3f7215367
mrkoc <mr.kocta@bk.ru>
2023-09-27 12:36:32 +0300

    (update): arg for create products, and load image of product

--- src/endpoints/product.py
+++ src/endpoints/product.py
@@ -1,10 +1,10 @@
 from src.models.product import Product
-from fastapi import APIRouter, Depends, HTTPException
-from src.schemas.product import ProductCreate
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form, Request
 from src.database import AsyncSession, get_async_session
 from src.models.user import User
 from src.auth.base_config import current_user
 from sqlalchemy import select
+from typing import Annotated
 
 router = APIRouter(
     prefix="/product",
@@ -33,11 +33,24 @@ async def get_current_product(product_id: int, db: AsyncSession = Depends(get_as
 
 
 @router.post("/add_product")
-async def add_product(product: ProductCreate, db: AsyncSession = Depends(get_async_session),
-                      user: User = Depends(current_user)):
+async def add_product(
+                      name: Annotated[str, Form()],
+                      description: Annotated[str, Form()],
+                      category: Annotated[str, Form()],
+                      price: Annotated[float, Form()],
+                      brand: Annotated[str, Form()], file: Annotated[UploadFile, File()] = None,
+                      db: AsyncSession = Depends(get_async_session),
+                      user: User = Depends(current_user),
+                      ):
+
+    image_path = file.filename
+    with open(f"product_images/{image_path}", "wb") as image_file:
+        image_file.write(file.file.read())
     async with db as session:
-        db_product = Product(**product.dict(), username=user.username)
+        db_product = Product(name=name, description=description, category=category, price=price, brand=brand,
+                             username=user.username, image_path=image_path)
         session.add(db_product)
         await db.commit()
         await db.refresh(db_product)
-        return db_product
+
+    return db_product
