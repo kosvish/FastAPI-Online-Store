be3fda63ea398e40a7d8673f2c617fa370d247cc
mrkoc <mr.kocta@bk.ru>
2023-09-27 13:49:16 +0300

    (style): fix intents and tabs, remove extra fields

--- src/pages/product_pages_router.py
+++ src/pages/product_pages_router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Request, Depends
-from src.models.user import User
 from src.endpoints.product import get_current_product
 from src.models.product import Product
-from .user_pages_router import templates, current_user
+from .user_pages_router import templates
 
 router = APIRouter(
     prefix="/product",
@@ -13,6 +12,4 @@ router = APIRouter(
 @router.get("/{product_id}")
 def get_product_page(request: Request, product: Product = Depends(get_current_product)):
     return templates.TemplateResponse("product.html", {"request": request, "product": product})
-
-
 
